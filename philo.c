75866f9d0fb848ca31e79ccdc4ffa9e96277c78f
Diogo Alexandre Carvalho Santos <dicarval@c1r11s2.42lisboa.com>
2024-11-06 17:36:35 +0000

    Creating the stuff

--- philo.c
+++ philo.c
@@ -6,23 +6,23 @@
 /*   By: dicarval <dicarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 11:38:41 by dicarval          #+#    #+#             */
-/*   Updated: 2024/10/31 16:25:57 by dicarval         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:59:08 by dicarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_philo *philo(void)
+philo_t	*data(void)
 {
-	static t_philo data;
+	static philo_t	data;
 
 	return(&data);
 }
 
 void	av_check(char **av)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 1;
 	while (av[i])
@@ -35,32 +35,93 @@ void	av_check(char **av)
 			else
 				error_hand(1);
 		}
-		if (ft_atoi(av[i]) >= INT_MAX || ft_atoi(av[i]) >= INT_MIN)
-		{
+		if (ft_atoi(av[i]) > INT_MAX || ft_atoi(av[i]) < INT_MIN)
 			error_hand(2);
-		}
 		i++;
 	}
 }
 
-void	av_parsing(char **av)
+void	av_allocation(char **av)
 {
 	av_check(av);
-	philo()->nbr_philo = ft_atoi(av[1]);
-	philo()->tt_die = ft_atoi(av[2]);
-	philo()->tt_eat = ft_atoi(av[3]);
-	philo()->tt_sleep = ft_atoi(av[4]);
+	data()->nbr_philo = ft_atoi(av[1]);
+	data()->tt_die = ft_atoi(av[2]);
+	data()->tt_eat = ft_atoi(av[3]);
+	data()->tt_sleep = ft_atoi(av[4]);
 	if (av[5])
-		philo()->nbr_tt_eat = ft_atoi(av[5]);
+		data()->nbr_tt_eat = ft_atoi(av[5]);
 	else
-		philo()->nbr_tt_eat = -1;
+		data()->nbr_tt_eat = -1;
+	data()->philo = malloc(sizeof(pthread_t)*(data()->nbr_philo));
+	if (data()->philo == NULL)
+		error_hand(3);
+	data()->forks = malloc(sizeof(pthread_mutex_t)*(data()->nbr_philo));
+	if (data()->forks == NULL)
+		error_hand(3);
+	data()->id = malloc(sizeof(int)*(data()->nbr_philo));
+	if (data()->id == NULL)
+		error_hand(3);
+}
 
+void	*eat_think_sleep_repeat(void *arg)
+{
+	int	i;
+	int	id;
+	int	right;
+	int	left;
+
+	id = *(int*)arg;
+	right = id;
+	left = (id + 1) % (data()->nbr_philo);
+	while(1)
+	{
+		if(id % 2 == 0)
+			pthread_mutex_lock(&(data()->forks[right]));
+		else
+			pthread_mutex_lock(&(data()->forks[left]));
+		if(id % 2 != 0)
+			pthread_mutex_lock(&(data()->forks[right]));
+		else
+			pthread_mutex_lock(&(data()->forks[left]));
+		pthread_mutex_lock(&(data()->message));
+		gettimeofday(, NULL);
+	}
+
+}
+
+void	thread_management(char **av)
+{
+	int	i;
+
+	i = 0;
+	data()->dead_philo = 0;
+	while(i < (data()->nbr_philo))
+		pthread_mutex_init(&(data()->forks[i++]), NULL);
+	pthread_mutex_init(&(data()->message), NULL);
+	i = 0;
+	while(i < (data()->nbr_philo))
+	{
+		data()->id[i] = i;
+		pthread_create(&(data()->philo[i]), NULL, eat_think_sleep_repeat, \
+		&data()->id[i++]);
+	}
+	i = 0;
+	while(i < (data()->nbr_philo))
+		pthread_join((data()->philo[i++]), NULL);
+	i = 0;
+	while(i < (data()->nbr_philo))
+		pthread_mutex_destroy(&(data()->forks[i++]));
+	pthread_mutex_destroy(&(data()->message));
+	ft_exit();
 }
 
 int	main(int ac, char **av)
 {
 	if (ac >= 5 && ac <= 6)
 	{
-		av_parsing(av);
+		av_allocation(av);
+		thread_management(av);
 	}
+	error_hand(0);
+	return (1);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: dicarval <dicarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:59:47 by dicarval          #+#    #+#             */
-/*   Updated: 2024/10/31 16:16:13 by dicarval         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:50:53 by dicarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,22 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/time.h>
 #include <limits.h>
 
-typedef struct	s_philo
+typedef struct	philo_s
 {
-	int	nbr_philo;
-	int	tt_die;
-	int	tt_eat;
-	int	tt_sleep;
-	int	nbr_tt_eat;
-}				t_philo;
+	int				nbr_philo;
+	int				tt_die;
+	int				tt_eat;
+	int				tt_sleep;
+	int				nbr_tt_eat;
+	int				dead_philo;
+	int				*id;
+	pthread_mutex_t	message;
+	pthread_mutex_t	*forks;
+	pthread_t		*philo;
+}				philo_t;
 
 
 #endif
